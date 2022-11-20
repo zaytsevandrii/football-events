26874d1bde22d6bb5870237f03357a647f160452
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-20 09:18:13 +0000

    create next app with pages

---
+++ pages/events/[anyEvents].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+export default function EventPage() {
+    const {query} = useRouter()
+  return (
+    <div>Any Events</div>
+  )
+}
+
---
+++ pages/events/index.js
@@ -0,0 +1,6 @@
+
+export default function index() {
+  return (
+    <div><h1>Events page</h1></div>
+  )
+}
